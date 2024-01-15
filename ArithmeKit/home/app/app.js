be23ade4855b849b6b869b09595a1bdd359be66d
Hydro <121924251+Lani202@users.noreply.github.com>
2024-01-15 19:05:10 +0800

    Add files via upload

---
+++ ArithmeKit/home/app/app.js
@@ -0,0 +1,30 @@
+function reload() {
+	window.location.reload();
+}
+
+// JavaScript to toggle visibility of bug icon and "Suggestions" link
+window.addEventListener("resize", function () {
+	toggleElementsVisibility();
+});
+
+// Initial check on page load
+window.addEventListener("DOMContentLoaded", function () {
+	toggleElementsVisibility();
+});
+
+function toggleElementsVisibility() {
+	const bugIcon = document.querySelector(".bx.bxs-bug");
+	const suggestionsLink = document.querySelector(
+		".navbar-nav > .nav-item.d-lg-none > .nav-link.text-white.text-opacity-50"
+	);
+
+	if (window.innerWidth >= 992) {
+		// Uncollapsed state
+		bugIcon.style.display = "inline-block";
+		suggestionsLink.style.display = "none";
+	} else {
+		// Collapsed state
+		bugIcon.style.display = "none";
+		suggestionsLink.style.display = "inline-block";
+	}
+}
